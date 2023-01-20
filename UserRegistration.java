144c0c064b18d2f952333bec3bcad34932e0a3e9
Sam-0517 <samadhankorde2000@gmail.com>
2023-01-20 14:14:17 +0530

    Added: code for Welcome Message of user Registration

---
+++ UserRegistration.java
@@ -0,0 +1,10 @@
+package com.bridgelabz.regex.usre_registration;
+
+public class UserRegistration {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("WELCOME TO USER REGISTRATION PROGRAM USING REGEX");
+	}
+
+}
